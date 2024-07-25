5341d220bd360c3c9ba3ba9f065fc06e2d0c5e81
Jil Magbulogtong <magbulogtong81@gmail.com>
2024-07-25 18:43:54 +0800

    remove vendor/twrp/config/gsm.mk entry

--- twrp_RE54D1.mk
+++ twrp_RE54D1.mk
@@ -15,7 +15,6 @@ $(call inherit-product, device/realme/RE54D1/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RE54D1
